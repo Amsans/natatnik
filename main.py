5ee03e8a18f1ba49fb6c6669abc1918fd0e245cf
Amsans <amsans@protonmail.com>
2025-06-20 20:54:36 +0300

    Fix cut/copy/paste for languages other than EN

--- main.py
+++ main.py
@@ -650,10 +650,22 @@ class TextEditor:
         self.fixed_tab_index = self.notebook.index("end") - 1
         return True
 
+def _onKeyRelease(event):
+    ctrl = (event.state & 0x4) != 0
+    if event.keycode == 88 and ctrl and event.keysym.lower() != "x":
+        event.widget.event_generate("<<Cut>>")
+
+    if event.keycode == 86 and ctrl and event.keysym.lower() != "v":
+        event.widget.event_generate("<<Paste>>")
+
+    if event.keycode == 67 and ctrl and event.keysym.lower() != "c":
+        event.widget.event_generate("<<Copy>>")
+
 
 def main():
     root = tk.Tk()
     TextEditor(root)
+    root.bind_all("<Key>", _onKeyRelease, "+")
     root.mainloop()
 
 
